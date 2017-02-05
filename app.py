77f00eff0d7536286db9d186a96d3d1aa89d9c11
Buka Cakrawala <kaka10garuda@gmail.com>
2017-02-05 14:59:37 -0800

    generate random word

--- app.py
+++ app.py
@@ -6,19 +6,35 @@ from sample import generate_probability, generate_word
 
 app = Flask(__name__)
 
+tes_dict = {'one': 1, 'blue': 1, 'two': 1, 'fish': 4, 'red': 1}
+
 dict_histogram = histogram('tom_sawyer.txt')
 
-tes_list = ["one", "fish", "two", "fish", "red", "fish", "blue", "fish"]
+
+def histogram_probability():
+    prob_dict = {}
+    for k in tes_dict.keys():
+        prob_dict[k] = (float(tes_dict[k]) / sum(tes_dict.values()))
+    # return stochastic_pick(prob_dict)
+    return stochastic_pick(prob_dict)
 
 
-def try_random():
-    return random.choice(tes_list)
+def stochastic_pick(dict_histogram):
+    # getting a random float from 0.0 to 1.0
+    rand_range = random.uniform(0, 1)
+    init_probability = 0.0
+    for k, v in dict_histogram.iteritems():
+        init_probability += v
+        if rand_range < init_probability:
+            break
+    return k
 
 
 @app.route('/')
 def hello_world():
-    return try_random()
+    return histogram_probability()
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+    # print stochastic_pick()
